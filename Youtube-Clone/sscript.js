f68a65b55c1b2dae53ec011cf7efe98168296601
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-01-06 09:38:49 +0500

    finished sidebar

--- Youtube-Clone/sscript.js
+++ Youtube-Clone/sscript.js
@@ -1,10 +1,12 @@
 var menuIcon = document.querySelector(".menu-icon");
-var sidebar = document.querySelector(".sidebar")
-var tabicons = document.querySelector(".topic-tabs-container")
+var sidebar = document.querySelector(".sidebar");
+var tabicons = document.querySelector(".topic-tabs-container");
+var maincontainer = document.querySelector(".main-container-section");
 
 
 
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
   tabicons.classList.toggle("tab-links");
+  maincontainer.classList.toggle("mainsection-toggle");
 }
